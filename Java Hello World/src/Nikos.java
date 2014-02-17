39dfaeb84852b49839a6d3518cf85fcf194bc73c
nikolis <nikolisgal@hotmail.com>
2014-02-17 15:51:28 +0200

    Update Nikos.java

--- Java Hello World/src/Nikos.java
+++ Java Hello World/src/Nikos.java
@@ -1,6 +1,8 @@
-
 public class Nikos {
 	public static void main(String[] args){
-		System.out.println("asdffdsfdsafdsa");
+		System.out.println("malakies");
+		System.out.println("malakies");
+		System.out.println("omalakiest");
+		System.out.println("malakiest");
 	}
 }
